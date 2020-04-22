ef18d2a69cd13db8f8623664b544be783193f3a3
RDegnen <rdegnen@gmail.com>
2020-04-22 09:39:34 -0400

    proxy done

--- src/proxy/index.ts
+++ src/proxy/index.ts
@@ -1,6 +1,13 @@
 import GumballMachine from "../state/gumball-machine"
 import GumballMonitor from "./gumball-monitor"
 
+/**
+ * The Proxy Pattern provides a surrogate or placeholder
+ * for another object to control access to it.
+ * 
+ * The code for this pattern in the book is really Java specific so
+ * not much is done here.
+ */
 function main() {
   const machine: GumballMachine = new GumballMachine('Sullivans Tap', 5)
   const monitor: GumballMonitor = new GumballMonitor(machine)
